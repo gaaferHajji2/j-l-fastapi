2cdfba2c213936dc89103cb4626a307f8c0e9e72
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-03-01 12:41:04 +0300

    Adding TestClient With client-Function AS Generator-Type

--- Social-Network-01/tests/conftest.py
+++ Social-Network-01/tests/conftest.py
@@ -5,3 +5,12 @@ import pytest
 from fastapi.testclient import TestClient
 
 from httpx import AsyncClient
+
+from main import app
+
+from routes.post import post_table
+
+from routes.comment import comment_table
+
+def client()-> Generator:
+    yield TestClient(app=app)
