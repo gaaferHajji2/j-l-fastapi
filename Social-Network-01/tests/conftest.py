56b954a468d1d4686ecdb8d3f5f11142102e981c
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-03-01 12:55:29 +0300

    Adding Test Functions To conftest.py

--- Social-Network-01/tests/conftest.py
+++ Social-Network-01/tests/conftest.py
@@ -12,6 +12,22 @@ from routes.post import post_table
 
 from routes.comment import comment_table
 
+@pytest.fixture(scope="session")
+def anyio_backend():
+    return "asyncio"
+
 @pytest.fixture()
-def client()-> Generator:
+def client() -> Generator:
     yield TestClient(app=app)
+
+@pytest.fixture(autouse=True)
+async def db() -> AsyncGenerator:
+    comment_table.clear()
+    post_table.clear()
+
+    yield post_table, comment_table
+
+@pytest.fixture()
+async def async_client(client) -> AsyncGenerator:
+    async with AsyncClient(app, base_url=client.base_url) as ac:
+        yield ac;
