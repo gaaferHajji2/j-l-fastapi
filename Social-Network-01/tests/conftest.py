7f549c48b6e658ac975e4c7cc28c92ee10cb1dbb
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-03-29 14:30:05 +0300

    Refactor Code
    Fix a bug about transport when using httpx AsyncClient With FastAPI-app
    
    Change the body of function to return status code and json-body
    
    Change Test URL To Avoid 304-status_code

--- Social-Network-01/tests/conftest.py
+++ Social-Network-01/tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 
 from fastapi.testclient import TestClient
 
-from httpx import AsyncClient
+from httpx import ASGITransport, AsyncClient
 
 from main import app
 
@@ -25,9 +25,9 @@ async def db() -> AsyncGenerator:
     comment_table.clear()
     post_table.clear()
 
-    yield post_table, comment_table
+    yield
 
 @pytest.fixture()
 async def async_client(client) -> AsyncGenerator:
-    async with AsyncClient(app, base_url=client.base_url) as ac:
+    async with AsyncClient(transport=ASGITransport(app=app),base_url=client.base_url) as ac:
         yield ac
--- Social-Network-01/tests/routers/test_post.py
+++ Social-Network-01/tests/routers/test_post.py
@@ -3,10 +3,18 @@ import pytest
 from httpx import AsyncClient
 
 async def create_post(body: str, async_client: AsyncClient):
-    response = await async_client.post(url='/post', json={"body": body})
+    response = await async_client.post(url='/post/', json={"body": body})
 
-    return response.json()
+    print("The Status Code Is: ", response.status_code)
+    print("The Response Body Is: ", response.json())
+
+    return response.status_code, response.json()
 
 @pytest.fixture()
 async def created_post(async_client: AsyncClient):
     return await create_post("J-L-Test-01 Post", async_client=async_client)
+
+@pytest.mark.anyio
+async def test_create_post(created_post):
+    assert created_post[0] == 201
+    assert {"body": "J-L-Test-01 Post"}.items() <= created_post[1].items()
