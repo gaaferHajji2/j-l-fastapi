f00ebc2f1451cc4a9fa2ee9954a927431c437952
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-03-31 13:08:08 +0300

    Adding Config Files With Variables

---
+++ Social-Network-01/config.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class BaseConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env', extra="ignore")
+
+class GlobalConfig(BaseConfig):
+    DATABASE_URL: Optional[str] = None
+    DB_FORCE_ROLL_BACK: bool = False
+    
