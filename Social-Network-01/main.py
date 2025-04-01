b46a62befaf08d55622de5de6eda0327519e3e37
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-04-01 13:21:02 +0300

    Adding asynccontextmanager And Define The LifeSpan Of FastAPI App

--- Social-Network-01/main.py
+++ Social-Network-01/main.py
@@ -1,9 +1,19 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from routes.post import router as post_router
 from routes.comment import router as comment_router
 
-app = FastAPI()
+from database import database
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await database.connect()
+    yield
+    await database.disconnect()
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(post_router, prefix="/post")
 
