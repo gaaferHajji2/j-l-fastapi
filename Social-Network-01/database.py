8685e00183d131f8ce8de6c8a3419e36da42f885
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-04-01 13:01:11 +0300

    Creating Tables, And Combine Them With Metadata-Object, Using databases-module

--- Social-Network-01/database.py
+++ Social-Network-01/database.py
@@ -6,10 +6,33 @@ from config import config
 
 metadata = sqlalchemy.MetaData()
 
+### Create The Posts Table ###
+post_table = sqlalchemy.Table(
+    "posts",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("body", sqlalchemy.String),
+)
+
+### Create The Comments Table ###
+comments_table = sqlalchemy.Table(
+    "comments",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("body", sqlalchemy.String),
+    # Here We Don't Need To Tell The Type Of Column, Because It's ForeignKey
+    # So It Will Give It The Same Type Of Id Of Posts Table.
+    sqlalchemy.Column("post_id", sqlalchemy.ForeignKey("posts.id"),nullable=False, ),
+)
+
+# connect_args={ "check_same_thread": False } --> This Only Required For Sqlite
 engine = sqlalchemy.create_engine(
     url = config.DATABASE_URL, connect_args={ "check_same_thread": False }
 )
 
+# Tell The Engine To Use The MetaData Object
+# So, When We Run The Code, All The Tables That Are Combine 
+# With The metadata-object, Will Be Created
 metadata.create_all(engine)
 
 database = databases.Database(
