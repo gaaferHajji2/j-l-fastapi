9155240d65b5920dff6451e7e693980c57a88ed3
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-04-04 13:27:06 +0300

    Settup Logging For Project
    Create Configuration File For Logging Information For Project
    
    Install Rich Module And Update requirements.txt File
    
    Update The Handler For The Logger

--- Social-Network-01/main.py
+++ Social-Network-01/main.py
@@ -2,15 +2,29 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+import logging
+
 from routes.post import router as post_router
 from routes.comment import router as comment_router
 
+from logging_conf import configure_logging
+
 from database import database
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+
+    configure_logging()
+
+    logger = logging.getLogger("social_network")
+
     await database.connect()
+
+    logger.info("Database Connected Successfully")
+
     yield
+    
     await database.disconnect()
 
 app = FastAPI(lifespan=lifespan)
