7590c784534fa98cc1178bdcb4007205c8e2bdcd
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-04-05 13:39:04 +0300

    Adding CorrelationId With Its Filter To Logging Configuration File

--- Social-Network-01/social_network/logging_conf.py
+++ Social-Network-01/social_network/logging_conf.py
@@ -6,17 +6,25 @@ def configure_logging() -> None:
     dictConfig({
         "version": 1,
         "disable_existing_loggers": False,
+        "filters": {
+            "correlation_id": {
+                "()": "asgi_correlation_id.CorrelationIdFilter",
+                # Here We Pass The Parameters To CorrelationIdFilter
+                "uuid_length": 8 if isinstance(config, DevConfig) else 32,
+                "default_value": "-"
+            }
+        },
         "formatters": {
             "console": {
                 "class": "logging.Formatter",
                 "datefmt": "%Y-%m-%dT%H:%M:%S",
-                "format": "%(name)s:%(lineno)d - %(message)s"
+                "format": "(%(correlation_id)s) %(name)s:%(lineno)d - %(message)s"
             },
 
             "file": {
                 "class": "logging.Formatter",
                 "datefmt": "%Y-%m-%dT%H:%M:%S",
-                "format": "%(asctime)s | %(levelname)-8s | %(name)s:%(lineno)d - %(message)s"
+                "format": "%(asctime)s | %(levelname)-8s | (%(correlation_id)s) %(name)s:%(lineno)d - %(message)s"
             },
         },
         "handlers": {
@@ -24,6 +32,7 @@ def configure_logging() -> None:
                 "class": "rich.logging.RichHandler",
                 "level": "DEBUG",
                 "formatter": "console",
+                "filters": ["correlation_id"],
             },
 
             "rotating_file": {
@@ -34,13 +43,15 @@ def configure_logging() -> None:
                 "maxBytes": 1024 * 1024, # 1MB
                 "backupCount": 2,
                 "encoding": "utf8",
+                "filters": ["correlation_id"],
             },
         },
         "loggers": {
             "social_network": {
                 "handlers": ["default", "rotating_file"],
                 "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
-                "propagate": False,
+                "propagate": False, # This Will Prevent From Sending Logs To Parent
+                # Note: The Main Parent For All Loggers Is Root
             },
 
             "uvicorn": {
