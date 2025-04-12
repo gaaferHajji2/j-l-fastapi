16156a962839cf9bc8533b54ac379c6a96d3757d
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-04-12 10:53:40 +0300

    Add First Test For Password Hashing And Verify

--- Social-Network-01/social_network/tests/security/test_security.py
+++ Social-Network-01/social_network/tests/security/test_security.py
@@ -1,6 +1,6 @@
 import pytest
 
-from social_network.security import get_user_by_email
+from social_network.security import get_user_by_email, verify_password, get_password_hash
 
 # print("The Name Is: ", __name__)
 
@@ -11,3 +11,8 @@ async def test_get_user(registered_user: dict):
     assert result is not None
 
     assert result.email == registered_user["email"]
+
+def test_password_hashing():
+    password = "password"
+
+    assert verify_password(password, get_password_hash(password))
