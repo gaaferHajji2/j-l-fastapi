f5367f9a55eea38c6093c787c3944e85990c7c45
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-04-12 11:12:29 +0300

    Create JWT Encoding Function

--- Social-Network-01/social_network/security.py
+++ Social-Network-01/social_network/security.py
@@ -2,11 +2,30 @@ import logging
 
 from passlib.context import CryptContext
 
+import datetime
+
+from jose import jwt
+
 from social_network.database import database, users_table
+
+from social_network.config import config
 
 logger = logging.getLogger(__name__)
 
 pwd_context = CryptContext(schemes=["bcrypt"])
+
+def create_access_token(email: str) -> str:
+    logger.debug("Creating Access Token", extra={"email": email})
+
+    expire = datetime.datetime.now(datetime.UTC) + datetime.timedelta(
+        minutes=30
+    )
+
+    jwt_data = {"sub": email, "exp": expire}
+
+    encoded_jwt = jwt.encode(jwt_data, config.SECRET_KEY, algorithm=config.ALGORITHM)
+
+    return encoded_jwt
 
 def get_password_hash(password: str):
     return pwd_context.hash(password)
