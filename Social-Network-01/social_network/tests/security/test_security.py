d1348ca7bafb4d699596109beeedd6c1d46bf14c
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-04-13 19:47:42 +0300

    Adding Tests For Create Token And Expired Token With mocker-Fixture

--- Social-Network-01/social_network/tests/security/test_security.py
+++ Social-Network-01/social_network/tests/security/test_security.py
@@ -1,8 +1,16 @@
 import pytest
 
-from social_network.security import get_user_by_email, verify_password, get_password_hash
+from httpx import AsyncClient
+
+from social_network.security import (
+    get_user_by_email,
+    verify_password,
+    get_password_hash,
+    create_access_token,
+)
 
 # print("The Name Is: ", __name__)
+
 
 @pytest.mark.anyio
 async def test_get_user(registered_user: dict):
@@ -12,7 +20,31 @@ async def test_get_user(registered_user: dict):
 
     assert result.email == registered_user["email"]
 
+
 def test_password_hashing():
     password = "password"
 
     assert verify_password(password, get_password_hash(password))
+
+
+@pytest.mark.anyio
+async def test_create_expired_token(
+    async_client: AsyncClient, registered_user: dict, mocker
+):
+    mocker.patch("social_network.security.get_token_expire_minutes", return_value=-1)
+
+    token = create_access_token(email=registered_user["email"])
+
+    response = await async_client.post(
+        "/post/",
+        json={
+            "body": "Test Body",
+        },
+        headers={"Authorization": f"Bearer {token}"},
+    )
+
+    print("The Json Response For Expired Token Is: ", response.json())
+
+    assert response.status_code not in (200, 201)
+
+    assert response.status_code == 401
