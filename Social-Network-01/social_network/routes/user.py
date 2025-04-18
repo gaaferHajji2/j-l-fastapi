07fe019a0e2c2933f0ef1ac9d6e6411ff73abcc9
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-04-18 18:17:09 +0300

    Adding Initial Route For Confimration

--- Social-Network-01/social_network/routes/user.py
+++ Social-Network-01/social_network/routes/user.py
@@ -7,10 +7,15 @@ from typing import Annotated
 import logging
 
 # from passlib.hash import pbkdf2_sha256
- 
+
 from social_network.models.user import UserIn
 
-from social_network.security import get_user_by_email, get_password_hash, authenticate_user
+from social_network.security import (
+    get_user_by_email,
+    get_password_hash,
+    authenticate_user,
+    get_subject_for_token_type,
+)
 
 from social_network.database import database, users_table
 
@@ -18,19 +23,19 @@ router = APIRouter()
 
 logger = logging.getLogger(__name__)
 
-@router.post('/register', status_code=201)
+
+@router.post("/register", status_code=201)
 async def register(user: UserIn):
     t1 = await get_user_by_email(user.email)
 
     if t1:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, 
-            detail="User Already Exists With That Email"
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User Already Exists With That Email",
         )
-    
+
     query = users_table.insert().values(
-        email=user.email, 
-        password=get_password_hash(user.password)
+        email=user.email, password=get_password_hash(user.password)
     )
 
     logger.debug(f"The Query For Creating User: {query}")
@@ -38,6 +43,7 @@ async def register(user: UserIn):
     result = await database.execute(query)
 
     return {"msg": "User Created Successfully", "id": result}
+
 
 # @router.post('/token', status_code=200)
 # async def login_user(user: UserIn):
@@ -48,14 +54,22 @@ async def register(user: UserIn):
 # @router.post('/token', status_code=200)
 # async def login_user(
 #     username: Annotated[str, Form()],
-#     password: Annotated[str, Form()], 
+#     password: Annotated[str, Form()],
 #     grant_type: Annotated[str, Form()]):
 #     access_token = await authenticate_user(username, password)
 
 #     return {"access_token": access_token, "token_type": "bearer"}
 
-@router.post('/token', status_code=200)
-async def login_user(form_data:  Annotated[OAuth2PasswordRequestForm, Depends()]):
+
+@router.post("/token", status_code=200)
+async def login_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     access_token = await authenticate_user(form_data.username, form_data.password)
 
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/confirm/{token}")
+async def confirm_user_email(token: str):
+    email = get_subject_for_token_type(token=token, token_type='confirmation')
+
+    
