4d4269cce2a3d522eb1498f966d479d49f502009
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-04-19 13:26:37 +0300

    Adding Email Task Using Gmail Provider

--- Social-Network-01/social_network/tasks.py
+++ Social-Network-01/social_network/tasks.py
@@ -6,5 +6,24 @@ from social_network.config import config
 
 import smtplib as smtp
 
-
 logger = logging.getLogger(__name__)
+
+async def send_simple_email(to: str, subject: str, body: str, from_: str):
+    sender = f"Private Person <{from_}>"
+    receiver = f"A Test User <{to}>"
+
+    message = f"""\
+        Subject: {subject}
+        To: {receiver}
+        From: {sender}
+
+        {body}"""
+    with smtp.SMTP(config.EMAIL_HOST, config.EMAIL_PORT) as server:
+
+        server.ehlo()
+
+        server.starttls()
+
+        server.login(config.USERNAME, config.PASSWORD)
+
+        server.sendmail(sender, receiver, message)
