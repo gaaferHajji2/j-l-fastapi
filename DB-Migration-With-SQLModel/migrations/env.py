6c33a582196c5c959eec5b37f5569b9ba6f98c68
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-07-05 10:56:13 +0300

    Update env file and generate first migration

--- DB-Migration-With-SQLModel/migrations/env.py
+++ DB-Migration-With-SQLModel/migrations/env.py
@@ -5,11 +5,15 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+from sqlmodel import SQLModel # Add By Jafar Loka
+
 from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+from migration.models.songs import Song # Add By Jafar Loka
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -20,7 +24,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata # Add By Jafar Loka
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
