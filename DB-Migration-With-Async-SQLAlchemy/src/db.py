42dd5d303c178a8214246011838ce7d4e205a9fe
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-07-12 11:59:11 +0300

    Updating Base With Constraints Names

--- DB-Migration-With-Async-SQLAlchemy/src/db.py
+++ DB-Migration-With-Async-SQLAlchemy/src/db.py
@@ -4,13 +4,24 @@ import os
 import datetime
 from typing import AsyncGenerator
 
-from sqlalchemy import DateTime
+from sqlalchemy import DateTime, MetaData
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
 load_dotenv()
 
 class Base(AsyncAttrs, DeclarativeBase):
+
+    metadata = MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
+
     type_annotation_map = {
         datetime.datetime : DateTime(timezone=True)
     }
