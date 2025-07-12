6238b090ae22b1fc4f5b3cfd8ff33cfc3d2436cd
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-07-12 11:31:57 +0300

    Define db with async session for asyncpg

---
+++ DB-Migration-With-Async-SQLAlchemy/src/db.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+
+import os
+
+import datetime
+
+from typing import AsyncGenerator
+
+from sqlalchemy import DateTime
+
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine, AsyncSession
+
+from sqlalchemy.orm import DeclarativeBase
+
+load_dotenv()
+
+class Base(AsyncAttrs, DeclarativeBase):
+    type_annotation_map = {
+        datetime.datetime : DateTime(timezone=True)
+    }
+
+engine = create_async_engine(os.environ.get("DATABASE_URL", ""))
+
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+async def get_async_db_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
