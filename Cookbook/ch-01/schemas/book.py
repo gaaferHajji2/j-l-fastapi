b5e5f7df6344bf1dacc53adde005ac0e504a708f
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-07-26 15:48:21 +0300

    Adding adv book with desc

--- Cookbook/ch-01/schemas/book.py
+++ Cookbook/ch-01/schemas/book.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from typing import Optional
 
@@ -6,3 +6,8 @@ class Book(BaseModel):
     title: str
     author: str
     year: Optional[int|str] = None
+
+class AdvancedBook(BaseModel):
+    title: str = Field(..., min_length=2, max_length=250, description="Title must be at least 2-chars")
+    author: str = Field(..., min_length=5, max_length=255, description="author must be at least 5-chars")
+    year: Optional[int|str] = Field(default=0, lt=200000, ge=2000, description="year must be between 2000 && 20000")
--- Cookbook/ch-01/src/app.py
+++ Cookbook/ch-01/src/app.py
@@ -5,7 +5,7 @@ from typing import Optional
 
 from enum import Enum
 
-from schemas.book import Book
+from schemas.book import Book, AdvancedBook
 
 app = FastAPI()
 
@@ -29,6 +29,14 @@ async def get_books_year(book_id: int, year: Optional[int] = None):
 
 @app.post("/books", response_model=Book)
 async def create_book(book: Book):
+    return {
+        "title": book.title,
+        "author": book.author,
+        "year": "Nothing" if book.year == None else book.year,
+    }
+
+@app.post("/adv/books", response_model=Book)
+async def create_advanced_book(book: AdvancedBook):
     return {
         "title": book.title,
         "author": book.author,
