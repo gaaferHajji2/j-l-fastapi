a14798158de0ff075872616e91417da522865de0
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-08-02 14:00:49 +0300

    Adding ruff, withe extra json body (not worked)

--- Cookbook/ch-01/schemas/book.py
+++ Cookbook/ch-01/schemas/book.py
@@ -2,12 +2,43 @@ from pydantic import BaseModel, Field
 
 from typing import Optional
 
+
 class Book(BaseModel):
     title: str
     author: str
-    year: Optional[int|str] = None
+    year: Optional[int | str] = None
+
 
 class AdvancedBook(BaseModel):
-    title: str = Field(..., min_length=2, max_length=250, description="Title must be at least 2-chars")
-    author: str = Field(..., min_length=5, max_length=255, description="author must be at least 5-chars")
-    year: Optional[int|str] = Field(default=0, lt=200000, ge=2000, description="year must be between 2000 && 20000")
+    title: str = Field(
+        ...,
+        min_length=2,
+        max_length=250,
+        description="Title must be at least 2-chars",
+        json_schema_extra={
+            "example": "test-01",
+            "description": "Title must be at least 2-chars",
+        },
+    )
+
+    author: str = Field(
+        ...,
+        min_length=5,
+        max_length=255,
+        description="author must be at least 5-chars",
+        json_schema_extra={
+            "example": "JLoka",
+            "description": "author must be at least 5-chars",
+        },
+    )
+
+    year: Optional[int | str] = Field(
+        default=0,
+        lt=200000,
+        ge=2000,
+        description="year must be between 2000 && 20000",
+        json_schema_extra={
+            "example": "2025",
+            "description": "year must be between 2000 && 20000",
+        },
+    )
--- Cookbook/ch-01/src/app.py
+++ Cookbook/ch-01/src/app.py
@@ -9,23 +9,24 @@ from schemas.book import Book, AdvancedBook
 
 app = FastAPI()
 
+
 class SortingEnum(str, Enum):
     desc = "desc"
-    asc  = "asc"
+    asc = "asc"
+
 
 @app.get("/books/{book_id}")
 async def get_books(book_id: int, sorting: Optional[SortingEnum] = None):
-    return {
-        "Book ID: ": book_id,
-        "sorting": sorting
-    }
+    return {"id: ": book_id, "sorting": sorting}
+
 
 @app.get("/books/{book_id}/year")
 async def get_books_year(book_id: int, year: Optional[int] = None):
     return {
-        "Book ID: " : book_id,
+        "id: ": book_id,
         "books": ["Book 01", "Book 02"] if year else ["All Books"],
     }
+
 
 @app.post("/books", response_model=Book)
 async def create_book(book: Book):
@@ -34,6 +35,7 @@ async def create_book(book: Book):
         "author": book.author,
         "year": "Nothing" if book.year == None else book.year,
     }
+
 
 @app.post("/adv/books", response_model=Book)
 async def create_advanced_book(book: AdvancedBook):
