9df7b908e98980a8ca4825e1b27b18b9492e8e94
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-08-02 14:13:01 +0300

    fix problem about error handling (worked)

--- Cookbook/ch-01/src/app.py
+++ Cookbook/ch-01/src/app.py
@@ -1,13 +1,23 @@
 import enum
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Request
 
 from typing import Optional
 
 from enum import Enum
 
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from pydantic import ValidationError
+
 from schemas.book import Book, AdvancedBook
 
 app = FastAPI()
+
+map_errors = {
+    'String should have at least 2 characters': 'Title must be at least 2-chars',
+    'String should have at least 5 characters': 'Author must be at least 5-chars',
+    'Input should be greater than or equal to 2000': 'Year must be between 2000 && 20000'
+}
 
 
 class SortingEnum(str, Enum):
@@ -44,3 +54,23 @@ async def create_advanced_book(book: AdvancedBook):
         "author": book.author,
         "year": "Nothing" if book.year == None else book.year,
     }
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, err: RequestValidationError):
+    errors = []
+
+    for error in err.errors():
+
+        print(f"error is: {err.__dict__}")
+
+        errors.append({
+            "field": error["loc"][-1] if error["loc"] else "unknown",
+            "message": map_errors.get(error["msg"], ''),
+            "desc": error.get("description", ''),
+            "type": error["type"]
+        })
+
+    return JSONResponse(
+        status_code=422,
+        content=errors
+    )
