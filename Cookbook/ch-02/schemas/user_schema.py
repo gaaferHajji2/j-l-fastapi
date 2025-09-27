3e38e3e2a02014b737f67ec98e1ca496d9736ff5
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-09-27 15:24:13 +0300

    Adding user create and list schema

---
+++ Cookbook/ch-02/schemas/user_schema.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, ConfigDict
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+
+class UserList(UserCreate):
+    id: int
+
+    model_config = ConfigDict(from_attributes=True)
