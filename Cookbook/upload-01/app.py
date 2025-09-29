c9d5dc37e51ac5a3a87ba4373364b76bf3e6e160
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-09-29 21:31:01 +0300

    complete example about file upload with aiofiles and python-multipart

--- Cookbook/upload-01/app.py
+++ Cookbook/upload-01/app.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI, File, UploadFile
+import aiofiles
+from pathlib import Path
+
+UPLOAD_FOLDER = Path("JLOKA")
+UPLOAD_FOLDER.mkdir(exist_ok=True)
 
 app = FastAPI()
 
@@ -9,5 +14,11 @@ async def getFile(file: UploadFile = File(...)):
     # t1 = file.tell() # Not valid now
 
     # await file.seek(0)
+
+    file_path = UPLOAD_FOLDER / file.filename # type: ignore
+
+    async with aiofiles.open(file_path, 'wb') as f:
+        content = await file.read()
+        await f.write(content)
 
     return { "file": file.filename, "size": file.size / 1024 if file.size is not None else 0}
