e0a1b829faf4242f3b90efe27e3c483e085c7154
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-11 04:48:44 +0300

    Adding user model && init async alembic && removed unused code

---
+++ Cookbook/TestOAuth/models/users.model.py
@@ -0,0 +1,21 @@
+from db import Base
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int]  = mapped_column(primary_key=True)
+
+    name: Mapped[str]
+
+    email: Mapped[str]
+
+    password: Mapped[str]
+
+    def to_json(self): 
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email
+        }
