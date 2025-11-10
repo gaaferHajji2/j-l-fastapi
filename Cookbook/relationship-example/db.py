997fb1c642c7692441eab20dce1395ed2113d7ff
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-11-10 05:32:08 +0300

    Finish from db with Base && metadata && AsyncSession && typing

--- Cookbook/relationship-example/db.py
+++ Cookbook/relationship-example/db.py
@@ -1,10 +1,34 @@
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncAttrs
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncAttrs, AsyncSession
 from sqlalchemy.orm import mapped_collection, Mapped, DeclarativeBase
+from sqlalchemy import MetaData, DateTime
 from dotenv import find_dotenv, load_dotenv
 
 import os
 import datetime
+from typing import AsyncGenerator
 
 load_dotenv(find_dotenv(filename='.env', raise_error_if_not_found=True), verbose=True)
 
+class Base(AsyncAttrs, DeclarativeBase):
+    metadata = MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
+
+    type_annotation_map = {
+        datetime.datetime : DateTime(timezone=True)
+    }
+
+
 engine = create_async_engine(os.environ.get('DATABASE_URL', ''), echo=True)
+
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+async def get_async_db_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
