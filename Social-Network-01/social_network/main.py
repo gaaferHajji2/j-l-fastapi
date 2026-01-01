4962e59e9940c3fa70f46a685228b8b33604931a
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-01 18:14:45 +0300

    Refactor main

--- Social-Network-01/social_network/main.py
+++ Social-Network-01/social_network/main.py
@@ -1,11 +1,7 @@
 from contextlib import asynccontextmanager
-
 from fastapi import FastAPI, HTTPException, Request
-
 from fastapi.exception_handlers import http_exception_handler
-
 import logging
-
 from asgi_correlation_id import CorrelationIdMiddleware
 
 from social_network.routes.post import router as post_router
@@ -13,40 +9,27 @@ from social_network.routes.comment import router as comment_router
 from social_network.routes.user import router as user_router
 from social_network.routes.like import router as like_router
 from social_network.routes.upload_files import router as upload_router
-
 from social_network.logging_conf import configure_logging
-
 from social_network.database import database
 
 # from fastapi.responses import JSONResponse
-
 logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-
     configure_logging()
-
     await database.connect()
-
     logger.info("Database Connected Successfully")
-
     yield
-
     await database.disconnect()
 
 app = FastAPI(lifespan=lifespan)
-
 app.add_middleware(CorrelationIdMiddleware)
 
 app.include_router(post_router, prefix="/post")
-
 app.include_router(comment_router, prefix="/comment")
-
 app.include_router(user_router, prefix='/user')
-
 app.include_router(like_router)
-
 app.include_router(upload_router)
 
 @app.exception_handler(HTTPException) # In This Way We Can Pass:
