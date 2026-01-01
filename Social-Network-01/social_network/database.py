80b130d5d846deac5bff0c043bcd7cc910f5dafb
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-01 17:54:18 +0300

    Refactor database

--- Social-Network-01/social_network/database.py
+++ Social-Network-01/social_network/database.py
@@ -1,11 +1,8 @@
 import databases
-
 import sqlalchemy
-
 from social_network.config import config
 
 metadata = sqlalchemy.MetaData()
-
 ### Create The Posts Table ###
 posts_table = sqlalchemy.Table(
     "posts",
@@ -15,7 +12,6 @@ posts_table = sqlalchemy.Table(
     sqlalchemy.Column("user_id", sqlalchemy.ForeignKey("users.id"), nullable=False),
     sqlalchemy.Column("image_url", sqlalchemy.String, nullable=True),
 )
-
 ### Create The Comments Table ###
 comments_table = sqlalchemy.Table(
     "comments",
@@ -27,7 +23,6 @@ comments_table = sqlalchemy.Table(
     sqlalchemy.Column("post_id", sqlalchemy.ForeignKey("posts.id"), nullable=False, ),
     sqlalchemy.Column("user_id", sqlalchemy.ForeignKey("users.id"), nullable=False),
 )
-
 ### Create The Users Table ###
 users_table = sqlalchemy.Table(
     "users",
@@ -37,7 +32,6 @@ users_table = sqlalchemy.Table(
     sqlalchemy.Column("password", sqlalchemy.String, nullable=False),
     sqlalchemy.Column("confirmed", sqlalchemy.Boolean, default=False),
 )
-
 ### Create The Likes Table ###
 likes_table = sqlalchemy.Table(
     "likes",
@@ -48,18 +42,14 @@ likes_table = sqlalchemy.Table(
     sqlalchemy.Column("post_id", sqlalchemy.ForeignKey("posts.id"), nullable=False, ),
     sqlalchemy.Column("user_id", sqlalchemy.ForeignKey("users.id"), nullable=False),
 )
-
-
 # connect_args={ "check_same_thread": False } --> This Only Required For Sqlite
 engine = sqlalchemy.create_engine(
     url = config.DATABASE_URL, connect_args={ "check_same_thread": False }
 )
-
 # Tell The Engine To Use The MetaData Object
 # So, When We Run The Code, All The Tables That Are Combine 
 # With The metadata-object, Will Be Created
 metadata.create_all(engine)
-
 database = databases.Database(
     url=config.DATABASE_URL, force_rollback=config.DB_FORCE_ROLL_BACK
 )
