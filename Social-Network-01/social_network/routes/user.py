439f7804d28c0b990576a1cb12f1ab8f7dfd34c9
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-03 12:14:16 +0300

    Refactor user routes

--- Social-Network-01/social_network/routes/user.py
+++ Social-Network-01/social_network/routes/user.py
@@ -1,15 +1,9 @@
 from fastapi import APIRouter, BackgroundTasks, HTTPException, status, Request, Depends
-
 from fastapi.security import OAuth2PasswordRequestForm
-
 from typing import Annotated
-
 import logging
-
 # from passlib.hash import pbkdf2_sha256
-
 from social_network.models.user import UserIn
-
 from social_network.security import (
     get_user_by_email,
     get_password_hash,
@@ -17,20 +11,15 @@ from social_network.security import (
     get_subject_for_token_type,
     create_confirm_token,
 )
-
 from social_network.database import database, users_table
-
 from social_network.tasks import send_user_registeration_email
 
 router = APIRouter()
-
 logger = logging.getLogger(__name__)
-
 
 @router.post("/register", status_code=201)
 async def register(user: UserIn, background_tasks: BackgroundTasks, request: Request):
     t1 = await get_user_by_email(user.email)
-
     if t1:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -41,11 +30,8 @@ async def register(user: UserIn, background_tasks: BackgroundTasks, request: Req
         email=user.email, password=get_password_hash(user.password),
         confirmed=False,
     )
-
     logger.debug(f"The Query For Creating User: {query}")
-
     result = await database.execute(query)
-
     background_tasks.add_task(
         send_user_registeration_email,
         email=user.email, 
@@ -56,7 +42,6 @@ async def register(user: UserIn, background_tasks: BackgroundTasks, request: Req
             )
         )
     )
-
     return {
         "msg": "User Created Successfully", 
         "id": result,
@@ -66,11 +51,9 @@ async def register(user: UserIn, background_tasks: BackgroundTasks, request: Req
         )
     }
 
-
 # @router.post('/token', status_code=200)
 # async def login_user(user: UserIn):
 #     access_token = await authenticate_user(user.email, user.password)
-
 #     return {"access_token": access_token, "token_type": "bearer"}
 
 # @router.post('/token', status_code=200)
@@ -79,25 +62,17 @@ async def register(user: UserIn, background_tasks: BackgroundTasks, request: Req
 #     password: Annotated[str, Form()],
 #     grant_type: Annotated[str, Form()]):
 #     access_token = await authenticate_user(username, password)
-
 #     return {"access_token": access_token, "token_type": "bearer"}
-
 
 @router.post("/token", status_code=200)
 async def login_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     access_token = await authenticate_user(form_data.username, form_data.password)
-
     return {"access_token": access_token, "token_type": "bearer"}
-
 
 @router.get("/confirm/{token}")
 async def confirm_user_email(token: str):
     email = get_subject_for_token_type(token=token, token_type='confirmation')
-
     query = users_table.update().where(users_table.c.email == email).values(confirmed=True)
-
     logger.debug(f"The Query For User Confirmation Is: {query}")
-
     await database.execute(query)
-
     return { "detail": "User Confirmed" }
