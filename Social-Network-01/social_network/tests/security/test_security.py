ac6967c85f1e83c102bcb40f5f1f0ab4037fab15
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-01-04 13:39:49 +0300

    Refactor test security

--- Social-Network-01/social_network/tests/security/test_security.py
+++ Social-Network-01/social_network/tests/security/test_security.py
@@ -1,5 +1,4 @@
 import pytest
-
 from httpx import AsyncClient
 
 from social_network.security import (
@@ -14,26 +13,19 @@ from social_network.security import (
 @pytest.mark.anyio
 async def test_get_user(registered_user: dict):
     result = await get_user_by_email(registered_user["email"])
-
     assert result is not None
-
     assert result.email == registered_user["email"]
-
 
 def test_password_hashing():
     password = "password"
-
     assert verify_password(password, get_password_hash(password))
-
 
 @pytest.mark.anyio
 async def test_create_expired_token(
     async_client: AsyncClient, registered_user: dict, mocker
 ):
     mocker.patch("social_network.security.get_token_expire_minutes", return_value=-1)
-
     token = create_access_token(email=registered_user["email"])
-
     response = await async_client.post(
         "/post/",
         json={
@@ -41,9 +33,6 @@ async def test_create_expired_token(
         },
         headers={"Authorization": f"Bearer {token}"},
     )
-
     print("The Json Response For Expired Token Is: ", response.json())
-
     assert response.status_code not in (200, 201)
-
     assert response.status_code == 401
