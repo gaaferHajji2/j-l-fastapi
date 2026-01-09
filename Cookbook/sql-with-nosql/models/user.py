0fe0e29b94041a7bf59dd971502ec533529d4271
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-09 15:00:42 +0300

    Refactor user model

--- Cookbook/sql-with-nosql/models/user.py
+++ Cookbook/sql-with-nosql/models/user.py
@@ -1,14 +1,10 @@
 from db import Base
-
 from sqlalchemy.orm import Mapped, mapped_column
 
 class User(Base):
     __tablename__ = 'users'
-
     id: Mapped[int]  = mapped_column(primary_key=True)
-
     name: Mapped[str]
-
     email: Mapped[str]
 
     def to_json(self): 
