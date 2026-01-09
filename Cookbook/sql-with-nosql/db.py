18d7b9ccd178568a382ee6c534c2bb1ef5014cfd
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-09 14:56:06 +0300

    Refactor db

--- Cookbook/sql-with-nosql/db.py
+++ Cookbook/sql-with-nosql/db.py
@@ -1,10 +1,9 @@
+import os
+import datetime
 from typing import AsyncGenerator
 from sqlalchemy import DateTime, MetaData
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
-
-import os
-import datetime
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(dotenv_path=find_dotenv(raise_error_if_not_found=True), verbose=True)
@@ -27,9 +26,7 @@ class Base(AsyncAttrs, DeclarativeBase):
 # print(f"The DATABASE_URL is: {os.environ.get('DATABASE_URL', 'NULL')}")
 
 engine = create_async_engine(os.environ.get("DATABASE_URL", ''), echo = True)
-
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
-
 async def get_async_db_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
