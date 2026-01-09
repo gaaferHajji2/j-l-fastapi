611f49cd07b3d1e5a655692c7b3661b8a1674e81
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-09 15:28:59 +0300

    Remove unused whitespaces from user schema

--- Cookbook/TestOAuth/schemas/user_schema.py
+++ Cookbook/TestOAuth/schemas/user_schema.py
@@ -4,7 +4,6 @@ class UserSchemaRes(BaseModel):
     id: int
     name: constr(min_length=1, max_length=255) # type: ignore
     email: EmailStr
-
     model_config = ConfigDict(from_attributes = True)
 
 class UserSchemaReq(BaseModel):
