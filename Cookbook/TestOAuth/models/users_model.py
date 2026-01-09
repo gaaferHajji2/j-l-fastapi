1b6d8377a26025f652a61a302b44c02f91f6a317
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-09 15:26:35 +0300

    Refactor users model

--- Cookbook/TestOAuth/models/users_model.py
+++ Cookbook/TestOAuth/models/users_model.py
@@ -4,7 +4,6 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 class User(Base):
     __tablename__ = 'users'
-
     id: Mapped[int]  = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(255), nullable=False)
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
