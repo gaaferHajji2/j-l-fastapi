3da6f21995b36ed26b97313df73e1a44ad988cda
JafarLoka <gaafer.hajji1995@gmail.com>
2026-01-09 15:08:10 +0300

    Refactor nosql users route

--- Cookbook/sql-with-nosql/routes/nosql_users.py
+++ Cookbook/sql-with-nosql/routes/nosql_users.py
@@ -1,15 +1,11 @@
 from typing import List
-
 from fastapi import APIRouter, HTTPException
-
 from bson import ObjectId
 
 from schemas.user_schema import UserCreate, NoSQLUserList
-
 from models.user_collection import user_collection
 
 nosql_user_router = APIRouter()
-
 
 @nosql_user_router.get("/", response_model=List[NoSQLUserList])
 async def get_all_users():
@@ -22,7 +18,6 @@ async def get_all_users():
         for user in user_collection.find()
     ]
 
-
 @nosql_user_router.post("/", response_model=NoSQLUserList)
 async def create_user(user: UserCreate):
     t1 = user_collection.insert_one(user.model_dump(exclude_none=True))
@@ -33,10 +28,7 @@ async def create_user(user: UserCreate):
 async def get_user(user_id: str):
     if not ObjectId.is_valid(user_id):
         raise HTTPException(status_code=400, detail={ "msg" : "user id not valid" })
-
     t1 = user_collection.find_one({ "_id": ObjectId(user_id) })
-
     if t1 is None:
         raise HTTPException(status_code=404, detail={ "msg": "No user found" })
-    
     return NoSQLUserList(id=str(t1["_id"]), name=t1["name"], email=t1["email"])
