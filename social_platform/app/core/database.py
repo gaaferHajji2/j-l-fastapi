d248169660d13b634031425a0b0d1a4beb3e5a84
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-13 13:27:41 +0300

    Finish from database and update target metadata

---
+++ social_platform/app/core/database.py
@@ -0,0 +1,42 @@
+import datetime
+from typing import AsyncGenerator
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import DateTime, MetaData
+from sqlalchemy.ext.asyncio import AsyncAttrs, AsyncSession, create_async_engine, async_sessionmaker
+from app.core.config import settings
+class Base(AsyncAttrs, DeclarativeBase):
+
+    metadata = MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
+
+    type_annotation_map = {
+        datetime.datetime : DateTime(timezone=True)
+    }
+
+engine = create_async_engine(
+    str(settings.DATABASE_URL),
+    echo=settings.DEBUG,
+    pool_size=settings.DATABASE_POOL_SIZE,
+    max_overflow=settings.DATABASE_MAX_OVERFLOW,
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
