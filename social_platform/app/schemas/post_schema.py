09ec69d62aa2c69e9e4d93733f44723e0628d52e
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-04-20 14:38:06 +0300

    Fix a typo issue & create post with author & comments & catgeories response schema

--- social_platform/app/schemas/post_schema.py
+++ social_platform/app/schemas/post_schema.py
@@ -1,6 +1,7 @@
-from pydantic import BaseModel, ConfigDict
+from typing import List
 from datetime import datetime
-
+from pydantic import BaseModel, ConfigDict
+from app.schemas.user_schema import UserResponse
 class PostResponse(BaseModel):
     id: int
     title: str
@@ -9,5 +10,12 @@ class PostResponse(BaseModel):
     is_published: bool
     created_at: datetime
     updated_at: datetime
+    
+    model_config = ConfigDict(from_attributes=True)
+
+class PostWithRelationsResponse(PostResponse):
+    author: UserResponse
+    comments: List["CommentResponse"] = []
+    categories: List["CategoryResponse"] = []
     
     model_config = ConfigDict(from_attributes=True)
