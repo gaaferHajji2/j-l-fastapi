6306c2068a28316310cbb99c059934b6003f45a6
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-11 04:41:15 +0300

    Create a connection to redis & fix cors middleware issue

--- Microservice-App-01/FastAPI-App/main.py
+++ Microservice-App-01/FastAPI-App/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection
 
 app = FastAPI()
 app.add_middleware(
-    CORSMiddleware(
-        allow_origins=['http://localhost:5173'],
-        allow_methods=['*'],
-        allow_headers=['*']
-    )
+    CORSMiddleware,
+    allow_origins=['http://localhost:5173'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+redis = get_redis_connection(
+    host = "localhost",
+    port = 6379,
+    decode_responses = True,
 )
