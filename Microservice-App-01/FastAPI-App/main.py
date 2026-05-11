cee8e577ab938e7f936e42ec360ba7db52123347
JafarLoka <gaafer.hajji1995@gmail.com>
2026-05-11 04:50:49 +0300

    Creating the redis models

--- Microservice-App-01/FastAPI-App/main.py
+++ Microservice-App-01/FastAPI-App/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from redis_om import get_redis_connection
+from redis_om import get_redis_connection, HashModel
 
 app = FastAPI()
 app.add_middleware(
@@ -14,3 +14,22 @@ redis = get_redis_connection(
     port = 6379,
     decode_responses = True,
 )
+
+class Delivery(HashModel):
+    budget: int = 0
+    notes: str = ''
+
+    class Meta:
+        database=redis
+
+class Event(HashModel):
+    delivery_id: str = None
+    type: str = ''
+    data: str = ''
+
+    class Meta:
+        database=redis
+
+@app.post('/deliveries/create')
+async def create_delivery(request: Request):
+    pass
